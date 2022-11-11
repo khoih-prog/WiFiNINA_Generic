4ca5a819f24a861a187b914d858f69a5aed8766f
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-11-11 15:21:41 -0500

    v1.8.14-7 to remove analogRead() from example
    ### Releases v1.8.14-7
    
    1. Modify [WiFiWebServer](https://github.com/khoih-prog/WiFiNINA_Generic/tree/master/examples/WiFiWebServer) example to avoid `analogRead()` crash in `arduino-pico` core. Check [WiFi.localIP() hangs in Nano RP2040 Connect with Arduino-Pico core (EarlePhilhower) #24](https://github.com/khoih-prog/WiFiNINA_Generic/discussions/24)
    2. Use `allman astyle` and add `utils`
    3. Update `Packages' Patches`

--- examples/WiFiWebServer/WiFiWebServer.ino
+++ examples/WiFiWebServer/WiFiWebServer.ino
@@ -60,127 +60,140 @@ WiFiServer server(80);
 
 void setup()
 {
-  //Initialize serial and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial && millis() < 5000);
+	//Initialize serial and wait for port to open:
+	Serial.begin(115200);
 
-  Serial.print(F("\nStart WiFiWebServer on ")); Serial.println(BOARD_NAME);
-  Serial.println(WIFININA_GENERIC_VERSION);
+	while (!Serial && millis() < 5000);
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE)
-  {
-    Serial.println(F("Communication with WiFi module failed!"));
-    // don't continue
-    while (true);
-  }
+	Serial.print(F("\nStart WiFiWebServer on "));
+	Serial.println(BOARD_NAME);
+	//Serial.println(WIFININA_GENERIC_VERSION);
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
-  {
-    Serial.print(F("Your current firmware NINA FW v"));
-    Serial.println(fv);
-    Serial.print(F("Please upgrade the firmware to NINA FW v"));
-    Serial.println(WIFI_FIRMWARE_LATEST_VERSION);
-  }
+	// check for the WiFi module:
+	if (WiFi.status() == WL_NO_MODULE)
+	{
+		Serial.println(F("Communication with WiFi module failed!"));
 
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED)
-  {
-    Serial.print(F("Attempting to connect to SSID: "));
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
+		// don't continue
+		while (true);
+	}
 
-    // wait 10 seconds for connection:
-    //delay(10000);
-  }
+	String fv = WiFi.firmwareVersion();
 
-  server.begin();
+	if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+	{
+		Serial.print(F("Your current firmware NINA FW v"));
+		Serial.println(fv);
+		Serial.print(F("Please upgrade the firmware to NINA FW v"));
+		Serial.println(WIFI_FIRMWARE_LATEST_VERSION);
+	}
+
+	// attempt to connect to Wifi network:
+	while (status != WL_CONNECTED)
+	{
+		Serial.print(F("Attempting to connect to SSID: "));
+		Serial.println(ssid);
+		// Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+		status = WiFi.begin(ssid, pass);
+
+		// wait 10 seconds for connection:
+		//delay(10000);
+	}
+
   // you're connected now, so print out the status:
   printWiFiStatus();
+
+	server.begin();
 }
 
 void loop()
 {
-  // listen for incoming clients
-  WiFiClient client = server.available();
-  if (client)
-  {
-    Serial.println(F("New client"));
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    
-    while (client.connected()) 
-    {
-      if (client.available()) 
-      {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) 
-        {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) 
-          {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          
-          client.println("</html>");
-          break;
-        }
-        
-        if (c == '\n') 
-        {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') 
-        {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
+#if 1
+	// listen for incoming clients
+	WiFiClient client = server.available();
 
-    // close the connection:
-    client.stop();
-    Serial.println(F("Client disconnected"));
-  }
+	if (client)
+	{
+		Serial.println(F("New client"));
+		// an http request ends with a blank line
+		boolean currentLineIsBlank = true;
+
+		while (client.connected())
+		{
+			if (client.available())
+			{
+				char c = client.read();
+				Serial.write(c);
+
+				// if you've gotten to the end of the line (received a newline
+				// character) and the line is blank, the http request has ended,
+				// so you can send a reply
+				if (c == '\n' && currentLineIsBlank)
+				{
+					// send a standard http response header
+					client.println("HTTP/1.1 200 OK");
+					client.println("Content-Type: text/html");
+					client.println("Connection: close");  // the connection will be closed after completion of the response
+					client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+					client.println();
+					client.println("<!DOCTYPE HTML>");
+					client.println("<html>");
+
+          int sensorReading;
+          
+					// output the value of each analog input pin
+					for (int analogChannel = 0; analogChannel < 6; analogChannel++)
+					{
+            // analogRead() will hang system in arduino-pico core
+						sensorReading = millis() % 1024;      //analogRead(analogChannel);
+						client.print("analog input ");
+						client.print(analogChannel);
+						client.print(" is ");
+						client.print(sensorReading);
+						client.println("<br />");
+					}
+
+					client.println("</html>");
+					break;
+				}
+
+				if (c == '\n')
+				{
+					// you're starting a new line
+					currentLineIsBlank = true;
+				}
+				else if (c != '\r')
+				{
+					// you've gotten a character on the current line
+					currentLineIsBlank = false;
+				}
+			}
+		}
+
+		// give the web browser time to receive the data
+		delay(1);
+
+		// close the connection:
+		client.stop();
+		Serial.println(F("Client disconnected"));
+	}
+#endif
 }
 
-void printWiFiStatus() 
+void printWiFiStatus()
 {
-  // print the SSID of the network you're attached to:
-  Serial.print(F("SSID: "));
-  Serial.println(WiFi.SSID());
+	// print the SSID of the network you're attached to:
+	Serial.print(F("SSID: "));
+	Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print(F("IP Address: "));
-  Serial.println(ip);
+	// print your board's IP address:
+	IPAddress ip = WiFi.localIP();
+	Serial.print(F("IP Address: "));
+	Serial.println(ip);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print(F("Signal strength (RSSI):"));
-  Serial.print(rssi);
-  Serial.println(F(" dBm"));
+	// print the received signal strength:
+	long rssi = WiFi.RSSI();
+	Serial.print(F("Signal strength (RSSI):"));
+	Serial.print(rssi);
+	Serial.println(F(" dBm"));
 }
